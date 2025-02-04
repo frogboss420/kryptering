ab478272f7e1f15824a77be86e1a86c95fac26bb
madsf <mads.friis.nielsen@gmail.com>
2025-02-04 13:46:49 +0100

    big progress nice

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-#wow
+alfabet = 'abcdefghijklmnopqrstuvwxyzæøå'
+nøglealfabet = ''
+
+#tvinger brugeren til at vælge en numerisk nøgle
+def vælgnøgle():
+    active = True
+    while active:
+        out = input('selekt nøgle\n')
+        if out.isnumeric() == False:
+            print('Valgte nøgle er ikke validt! Vælg et nummer!')
+        else:
+            print('thank')
+            return int(out)
+
+nøgle = vælgnøgle()
+
+#main løkke
+mainloop = True
+while mainloop:
+    decision = input('ENKRYPT, DEKRYPT, vælg NØGLE eller EXIT.')
+    decision = str(decision.upper())
+    if decision == 'ENKRYPT':
+        print('yay')
+    elif decision == 'DEKRYPT':
+        print('yayy')
+    elif decision == 'NØGLE':
+        nøgle = vælgnøgle()
+    elif decision == 'EXIT':
+        break
+    else:
+        print('man what the hell man')
